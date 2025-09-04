ab0cd29bb41e868438560965e0f4a959daccd585
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-09-04 11:50:26 -0300

    feat: create API

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,25 +1,11 @@
 import express from "express";
 
-// Importar todas as rotas
-import authRouter from "./auth.routes.js";
-import animesRouter from "./animeRoutes.js";
-import personagensRouter from "./personagemRoutes.js";
-import collectionRouter from "./collectionRoutes.js";
-import cardRouter from "./cardRoutes.js";
-
-import authMiddleware from "../middleware/authMiddleware.js";
+// Importe da rota
+import pontosRouter from "./pontoTuristicoRoutes.js";
 
 const router = express.Router();
 
-// Rotas públicas
-router.use("/auth", authRouter);
-router.use("/collections", collectionRouter);
-router.use("/cards", cardRouter);
-
-// Rotas protegidas
-router.use(authMiddleware);
-
-router.use("/animes", animesRouter);
-router.use("/personagens", personagensRouter);
+// Rota pública
+router.use("/pontos-turisticos", pontosRouter);
 
 export default router;
